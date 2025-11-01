c901f4c87e276098ab615a4596865389b95e5e7f
schnitzel <schnitzel@schnitzel.schnitzel>
2025-10-31 23:24:34 -0700

    modified:   src/main/java/com/schnitzel/Blockchain/entity/MedicalRecord.java

--- src/main/java/com/schnitzel/Blockchain/entity/MedicalRecord.java
+++ src/main/java/com/schnitzel/Blockchain/entity/MedicalRecord.java
@@ -1,5 +1,46 @@
 package com.schnitzel.Blockchain.entity;
 
+import org.apache.milagro.amcl.RSA2048.private_key;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table (name = "medicalrecord")
 public class MedicalRecord {
+    
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Long age;
+    private String address;
+    private String gender;
+
+    private String medicalHistory;
+    private String previousBlockHash;
+
+    private String publicKey;
+    private String privateKey;
+
+
+    public MedicalRecord(){
+    }
+
+    public MedicalRecord(String firstName, String middleName, String lastName,String lastName, Long age, String address,String gender ){
+
+    }
+
     
 }
