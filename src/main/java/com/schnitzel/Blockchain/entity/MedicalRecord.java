51c690c06df6312eb054829e1d59e6710a881597
schnitzel <schnitzel@schnitzel.schnitzel>
2025-10-31 22:57:18 -0700

    new file:   .gitattributes 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/schnitzel/Blockchain/BlockchainApplication.java 	new file:   src/main/java/com/schnitzel/Blockchain/entity/Block.java 	new file:   src/main/java/com/schnitzel/Blockchain/entity/MedicalRecord.java 	new file:   src/main/java/com/schnitzel/Blockchain/entity/Patient.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/schnitzel/Blockchain/BlockchainApplicationTests.java

---
+++ src/main/java/com/schnitzel/Blockchain/entity/MedicalRecord.java
@@ -0,0 +1,5 @@
+package com.schnitzel.Blockchain.entity;
+
+public class MedicalRecord {
+    
+}
---
+++ src/test/java/com/schnitzel/Blockchain/BlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schnitzel.Blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
