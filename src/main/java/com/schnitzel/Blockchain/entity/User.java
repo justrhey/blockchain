2b9c7d4e5700c976a3e692814b6b38e3420d2cef
schnitzel <schnitzel@schnitzel.schnitzel>
2025-11-03 00:29:02 -0800

    modified:   src/main/java/com/schnitzel/Blockchain/entity/User.java 	new file:   src/main/java/com/schnitzel/Blockchain/repository/MedicalRecordRepositor.java

--- src/main/java/com/schnitzel/Blockchain/entity/User.java
+++ src/main/java/com/schnitzel/Blockchain/entity/User.java
@@ -3,5 +3,25 @@ package com.schnitzel.Blockchain.entity;
 public class User {
 
     //TO DO: User accounts for doctors etc
+    private String firstName;
+    private String email;
+    private String password;
+    private String middleName;
+    private String lastName;
+
+    public User(){
+    }
+
+    public User(String firstName, String email, String password, String middleName, String lastName){   
+        this.firstName = firstName;
+        this.email = email;
+        this.password = password;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
     
 }
